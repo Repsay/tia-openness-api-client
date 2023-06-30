f8c1792cbd10ceea078052357512970f39a4d514
Jasper Delahaije <47220315+Repsay@users.noreply.github.com>
2023-06-30 10:04:18 +0200

    Update setup.py
    Change version to 3.11

--- setup.py
+++ setup.py
@@ -15,9 +15,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.11'
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.11',
 )
